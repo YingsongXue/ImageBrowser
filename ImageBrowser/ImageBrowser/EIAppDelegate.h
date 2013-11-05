8eaaf9332777ac9c7ef29a40ee446f9a05a74bc3
mucun1987 <liangyanan@yanantekiMacBook-Pro.local>
2013-11-05 17:21:30 +0800

    Lib
    Lib

---
+++ ImageBrowser/ImageBrowser/EIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  EIAppDelegate.h
+//  ImageBrowser
+//
+//  Created by 亚南 on 13-11-5.
+//  Copyright (c) 2013年 Encompass. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ImageBrowser/ImageBrowser/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ImageBrowser
+//
+//  Created by 亚南 on 13-11-5.
+//  Copyright (c) 2013年 Encompass. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "EIAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([EIAppDelegate class]));
+    }
+}
