2336b78b952197ab17780735a950ac61a81eb9a2
mucun1987 <liangyanan@yanantekiMacBook-Pro.local>
2013-11-05 17:28:45 +0800

    add viewcontroller

---
+++ ImageBrowser/ImageBrowser/EIMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  EIMainViewController.h
+//  ImageBrowser
+//
+//  Created by 亚南 on 13-11-5.
+//  Copyright (c) 2013年 Encompass. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EIMainViewController : UIViewController
+
+@end
